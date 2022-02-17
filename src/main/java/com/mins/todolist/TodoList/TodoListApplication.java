55e7d4654e4d841645897962bb5829c1d4e24281
min <www5981@naver.com>
2022-02-17 17:04:40 +0900

    datasource 수정

--- src/main/java/com/mins/todolist/TodoList/TodoListApplication.java
+++ src/main/java/com/mins/todolist/TodoList/TodoListApplication.java
@@ -2,6 +2,7 @@ package com.mins.todolist.TodoList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
